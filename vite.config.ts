b0dac86104c93de5bfc173a4c263ce84badf91ac
Yaroslav Romanenko <yarikrom05@gmail.com>
2023-02-26 18:23:07 +0200

    feat: add vite basic configuration for lib building

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    build: {
+        cssCodeSplit: true,
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            name: 'paw-ui',
+            fileName: 'index',
+            formats: ['es'],
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom'],
+            input: {
+                index: path.resolve(__dirname, 'src/index.ts'),
+                theme: path.resolve(__dirname, 'src/styles/theme.scss'),
+            },
+            output: {
+                assetFileNames: (assetInfo) => (assetInfo.name === 'index.css' ? 'paw-ui.css' : assetInfo.name),
+            },
+        },
+    },
+    plugins: [react(), dts()],
+});
